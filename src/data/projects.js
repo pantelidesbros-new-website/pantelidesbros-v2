e0fa77a6ec22f25b63d49997621c6b0071a1761e
George Nalmpantis <george@phemegrowth.com>
2025-12-02 07:33:55 +0200

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,19 +2,19 @@ export const projects = [
   {
     id: 1,
     title: "Project 1",
-    image: "/src/assets/project 1.jpg",
+    image: "/images/project 1.JPG",
     description: "Description for project 1."
   },
   {
     id: 2,
     title: "Project 2",
-    image: "/src/assets/project 2.png",
+    image: "/images/project 2.png",
     description: "Description for project 2."
   },
   {
     id: 3,
     title: "Project 3",
-    image: "/src/assets/project 3.png",
+    image: "/images/project 3.png",
     description: "Description for project 3."
   }
 ];
